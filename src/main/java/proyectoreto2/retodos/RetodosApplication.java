ab4a20cb300c915a096f9f9012061be48218d1de
Eduar Ramírez <eduar.ramirez.mt@correo.usa.edu.co>
2021-10-16 00:00:24 -0500

    Archivos reto 2

---
+++ src/main/java/proyectoreto2/retodos/RetodosApplication.java
@@ -0,0 +1,13 @@
+package proyectoreto2.retodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetodosApplication.class, args);
+	}
+
+}
